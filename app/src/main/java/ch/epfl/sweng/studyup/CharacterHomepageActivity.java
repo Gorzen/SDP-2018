77bdc3ee5d8654bc92414f2cb11fb168e161ad10
Jules Gottraux <jules.gottraux@epfl.ch>
2018-09-29 19:25:47 +0200

    Add player instance and button to gain xp
    Implementation of the player is a first sketch and isn't a good
    way of handling the instance of Player, will have to make this
    cleaner.

--- app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
@@ -2,17 +2,21 @@ package ch.epfl.sweng.studyup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import az.plainpie.PieView;
 import az.plainpie.animation.PieAngleAnimation;
 
 public class CharacterHomepageActivity extends AppCompatActivity {
+    private Player player;
+    public static final int XP_STEP = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_homepage);
         pieViewAnimation();
+        player = new Player();
     }
 
     /**
@@ -23,5 +27,9 @@ public class CharacterHomepageActivity extends AppCompatActivity {
         PieAngleAnimation animation = new PieAngleAnimation(pieView);
         animation.setDuration(5000); //This is the duration of the animation in millis
         pieView.startAnimation(animation);
+    }
+
+    public void addExpPlayer(View view) {
+        player.addExperience(XP_STEP);
     }
 }
