0acde926251d3d5e93c498007c45461e66834406
Nick Heyek <nheyek@pm.me>
2018-10-03 18:15:24 +0200

    Implement button press for coordinates
    This commit includes the following:
    
    - A class, LocationTracker, which uses the android library for accessing
    the phone's GPS data
    - A button in the main activity, with a callback which creates an
    instance of the afformentioned class, retrieves the coordinates, and
    displays them to the user

--- app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.studyup;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.location.Location;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(
+                MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                123);
     }
 
 
     public void toCharacterHomePage(View view) {
         Intent intent = new Intent(this, CharacterHomepageActivity.class);
         startActivity(intent);
+    }
+
+    public void onLocButtonClick(View view) {
+        LocationTracker locationTracker = new LocationTracker(getApplicationContext());
+        Location currLoc = locationTracker.getLocation();
+        if (currLoc != null) {
+            double currLat = currLoc.getLatitude();
+            double currLong = currLoc.getLongitude();
+
+            Toast.makeText(getApplicationContext(),
+                    "Latitude: " + currLat + "\nLongitude: " + currLong,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
