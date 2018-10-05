a1cb62c9e60f9b30a45b6771e1b03fee2d248b72
Jules Gottraux <jules.gottraux@epfl.ch>
2018-10-05 21:34:31 +0200

    Add of new player's progression display
    Add of the new library and basic display using it for the player's
    level progression

--- app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
@@ -4,17 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator;
+
 
 public class CharacterHomepageActivity extends AppCompatActivity {
-    public static final int XP_STEP = 10;
+    CircularProgressIndicator levelProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_homepage);
+        levelProgress = findViewById(R.id.level_progress);
+        levelProgress.setProgress(Player.get().getLevelProgress(), 1);
+        levelProgress.setStartAngle(270);
     }
 
     public void addExpPlayer(View view) {
-        Player.get().addExperience(XP_STEP);
+        Player.get().addExperience(Player.XP_STEP);
+        levelProgress.setCurrentProgress(Player.get().getLevelProgress());
     }
 }
