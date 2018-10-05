2220c348231d24a7b956ba38fd83068f4856601d
Jules Gottraux <jules.gottraux@epfl.ch>
2018-10-05 21:32:55 +0200

    Delete useless basic features
    Delete Hello World layout and corresponding test

--- app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void toCharacterHomePage(View view) {
         Intent intent = new Intent(this, CharacterHomepageActivity.class);
         startActivity(intent);
