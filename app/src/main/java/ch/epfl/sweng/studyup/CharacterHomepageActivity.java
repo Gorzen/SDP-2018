11cb5513a38fd9914e5f6f1ea3138db2d347ab6d
Jules Gottraux <jules.gottraux@epfl.ch>
2018-10-07 19:52:31 +0200

    Change of text displayed in layout
    Also delete empty MainActivityTest

--- app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/CharacterHomepageActivity.java
@@ -6,9 +6,19 @@ import android.view.View;
 
 import antonkozyriatskyi.circularprogressindicator.CircularProgressIndicator;
 
+import static java.sql.DriverManager.println; //DEBUG
+
 
 public class CharacterHomepageActivity extends AppCompatActivity {
     CircularProgressIndicator levelProgress;
+
+    //Texte that will be displayed in the levelProgress layout
+    private static final CircularProgressIndicator.ProgressTextAdapter LEVEL_PROGRESS_TEXT = new CircularProgressIndicator.ProgressTextAdapter() {
+        @Override
+        public String formatText(double progress) {
+            return (progress*100+"% of level ").concat(String.valueOf(Player.get().getLevel()+1));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +27,12 @@ public class CharacterHomepageActivity extends AppCompatActivity {
         levelProgress = findViewById(R.id.level_progress);
         levelProgress.setProgress(Player.get().getLevelProgress(), 1);
         levelProgress.setStartAngle(270);
+        levelProgress.setProgressTextAdapter(null);
     }
 
     public void addExpPlayer(View view) {
         Player.get().addExperience(Player.XP_STEP);
         levelProgress.setCurrentProgress(Player.get().getLevelProgress());
+        levelProgress.setProgressTextAdapter(LEVEL_PROGRESS_TEXT);
     }
 }
