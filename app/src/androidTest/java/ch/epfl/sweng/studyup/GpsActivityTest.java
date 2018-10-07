307f8e8b6ec9dbcffc9cb2ef0626f71f4f959995
Lucien Michael Iseli <lucien.iseli@epfl.ch>
2018-10-07 15:40:34 +0200

    Check if mock locations disabled

--- app/src/androidTest/java/ch/epfl/sweng/studyup/GpsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/GpsActivityTest.java
@@ -8,8 +8,6 @@ import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,45 +20,41 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class GpsActivityTest {
-    private double latitude = 10.0;
-    private double longitude = 20.0;
-    private LocationManager lm = null;
 
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void prepare(){
-        double latitude = 10.0;
-        double longitude = 20.0;
-
-        lm = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        if(lm.getAllProviders().contains("test")){
-            lm.removeTestProvider("test");
-        }
-
-        lm.addTestProvider("test", false, false, false, false, false, false, false, Criteria.POWER_HIGH, Criteria.ACCURACY_HIGH);
-        lm.setTestProviderEnabled("test", true);
-
-        Location location = new Location("test");
-        location.setLatitude(latitude);
-        location.setLongitude(longitude);
-        location.setAccuracy(1);
-        location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
-        location.setTime(System.currentTimeMillis());
-        lm.setTestProviderLocation("test", location);
-    }
-
     @Test
-    public void testGPSActivity(){
-        onView(withId(R.id.locButton)).perform(click());
+    public void GpsTest(){
+        try {
+            double latitude = 10.0;
+            double longitude = 20.0;
 
-        onView(withId(R.id.helloWorld)).check(matches(withText("Latitude: " + latitude + "\nLongitude: " + longitude)));
-    }
+            LocationManager lm = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-    @After
-    public void cleanUp(){
-        lm.removeTestProvider("test");
+            if (lm.getAllProviders().contains("test")) {
+                lm.removeTestProvider("test");
+            }
+
+            lm.addTestProvider("test", false, false, false, false, false, false, false, Criteria.POWER_HIGH, Criteria.ACCURACY_HIGH);
+            lm.setTestProviderEnabled("test", true);
+
+            Location location = new Location("test");
+            location.setLatitude(latitude);
+            location.setLongitude(longitude);
+            location.setAccuracy(1);
+            location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+            location.setTime(System.currentTimeMillis());
+            lm.setTestProviderLocation("test", location);
+
+
+            onView(withId(R.id.locButton)).perform(click());
+            onView(withId(R.id.helloWorld)).check(matches(withText("Latitude: " + latitude + "\nLongitude: " + longitude)));
+
+
+            lm.removeTestProvider("test");
+        }catch(SecurityException e){
+
+        }
     }
 }
