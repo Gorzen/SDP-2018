a8bd62847f60e07e022ca529c71893a169083f21
lucasstrauss <lucas.strauss@epfl.ch>
2018-10-09 22:52:44 +0200

    Remove "====" line in MainActivity

--- app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import ch.epfl.sweng.studyup.question.AddQuestionActivity;
-=======
 import android.widget.TextView;
 import android.widget.Toast;
 
