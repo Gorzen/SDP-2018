2c6d7dfd171c8ee41f0d588d2b6b54c53bf02ecc
Nick Heyek <nickheyek@protonmail.com>
2018-10-12 12:59:47 +0200

    Print more error data

--- app/src/main/java/ch/epfl/sweng/studyup/AuthenticationActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/AuthenticationActivity.java
@@ -18,26 +18,27 @@ import org.json.*;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
-    private void reportAuthError(String error) {
+    private void reportAuthError() {
 
-        Log.e(getString(R.string.auth_error), error);
         Toast.makeText(AuthenticationActivity.this,
                 "Unable to authenticate.",
                 Toast.LENGTH_SHORT).show();
     }
 
     private String getTokenFromResponse(String response) throws JSONException {
+
         JSONObject tokenResponseJSON = new JSONObject(response);
 
-        if (tokenResponseJSON.has("error")) {
-            String error = tokenResponseJSON.getString("error");
-            reportAuthError(error);
-            return null;
-        }
-        else {
+        if (tokenResponseJSON.has("access_token")) {
             String token = tokenResponseJSON.getString("access_token");
             return token;
         }
+        if (tokenResponseJSON.has("error")) {
+            String error = tokenResponseJSON.getString("error");
+            reportAuthError();
+            Log.e(getString(R.string.auth_error), error);
+        }
+        return null;
     }
 
     private String getToken(String code) throws IOException, JSONException {
@@ -53,23 +54,36 @@ public class AuthenticationActivity extends AppCompatActivity {
     private void displayProfileDataFromResponse(String response) throws JSONException {
 
         JSONObject profileResponseJSON = new JSONObject(response);
-        if (profileResponseJSON.has("error")) {
-            String error = profileResponseJSON.getString("error");
-            reportAuthError(error);
-        }
-        else {
+
+        if (profileResponseJSON.has("Firstname") && profileResponseJSON.has("Sciper")) {
             String firstName = profileResponseJSON.getString("Firstname");
             String sciperNumber = profileResponseJSON.getString("Sciper");
 
             TextView profileDataDisplay = findViewById(R.id.profileDataDisplay);
             profileDataDisplay.setText("Welcome, " + firstName + ".\nYour Sciper number is " + sciperNumber + ".");
         }
+        if (profileResponseJSON.has("error")) {
+            String error = profileResponseJSON.getString("error");
+            reportAuthError();
+            Log.e(getString(R.string.auth_error), error);
+        }
     }
 
     private void displayProfileData(String token) throws IOException, JSONException {
 
+        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0");
+
         String profileUrl = "https://tequila.epfl.ch/cgi-bin/OAuth2IdP/userinfo" + "?access_token=" + token;
         HttpURLConnection profConnection = (HttpURLConnection) new URL(profileUrl).openConnection();
+
+        int profConnectionResponseCode = profConnection.getResponseCode();
+        if (profConnectionResponseCode >= 400) {
+            System.out.println("ERROR, SERVER RESPONSE CODE: " + profConnectionResponseCode);
+            InputStream errorStream = profConnection.getInputStream();
+            String errorResponse = new Scanner(errorStream).useDelimiter("\\A").next();
+            System.out.println(errorResponse);
+        }
+
         InputStream profStream = profConnection.getInputStream();
         String profileResponse = new Scanner(profStream).useDelimiter("\\A").next();
 
@@ -91,7 +105,8 @@ public class AuthenticationActivity extends AppCompatActivity {
         String error = authCodeURI.getQueryParameter("error");
 
         if (!TextUtils.isEmpty(error)) {
-            reportAuthError(error);
+            reportAuthError();
+            Log.e(getString(R.string.auth_error), error);
         }
         else {
             try {
@@ -100,6 +115,7 @@ public class AuthenticationActivity extends AppCompatActivity {
                     displayProfileData(token);
                 }
             } catch (Exception e) {
+                reportAuthError();
                 e.printStackTrace();
             }
         }
