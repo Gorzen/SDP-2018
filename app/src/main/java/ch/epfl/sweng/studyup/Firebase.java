b6dba1e19665e75871e180d09f8d5df84c4152d2
Stache <hugo.hueber@epfl.ch>
2018-10-13 17:05:38 +0200

    Set user data

--- app/src/main/java/ch/epfl/sweng/studyup/Firebase.java
+++ app/src/main/java/ch/epfl/sweng/studyup/Firebase.java
@@ -3,13 +3,12 @@ package ch.epfl.sweng.studyup;
 import android.support.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Map;
 
@@ -17,6 +16,7 @@ public class Firebase {
     private static Firebase instance = null;
     private static FirebaseFirestore db;
     private static Map<String, Object> userData;
+    private static boolean lastActionSuccess;
 
     private Firebase() {
         // Access a Cloud Firestore instance from your Activity
@@ -39,7 +39,7 @@ public class Firebase {
 
     public Map<String, Object> getUserData(final int sciper) throws IllegalArgumentException {
         if(sciper < 100000 || sciper > 999999) {
-            throw new IllegalArgumentException("Error: SCIPER number should be a six digits number.");
+            throw new IllegalArgumentException("Error: getUserData, SCIPER number should be a six digits number.");
         }
 
         db.collection("users").document(Integer.toString(sciper))
@@ -51,16 +51,44 @@ public class Firebase {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         userData = document.getData();
+                        System.out.println("Success: getUserData(" + sciper + ").");
                     } else {
-                        System.out.println("Error: No such user (" + sciper + ").");
+                        System.out.println("Error: getUserData, no such user (" + sciper + ").");
                     }
                 } else {
-                    System.out.println("get failed with " + task.getException());
+                    System.out.println("Error: getUserData, " + task.getException());
                 }
             }
         });
 
         return userData;
+    }
+
+    public boolean setUserData(final int sciper, final String key, final Object value) throws IllegalArgumentException {
+        if (sciper < 100000 || sciper > 999999) {
+            throw new IllegalArgumentException("Error: setUserData, SCIPER number should be a six digits number.");
+        }
+
+        Map<String, Object> data = getUserData(sciper);
+        data.put(key, value);
+
+        db.collection("users").document(Integer.toString(sciper))
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        lastActionSuccess = true;
+                        System.out.println("Succes: setUserData(" + sciper + ").");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        lastActionSuccess = false;
+                        System.out.println("Error: setUserData, " + e.toString());
+                    }
+                });
+        return lastActionSuccess;
     }
 }
 
--- app/src/test/java/ch/epfl/sweng/studyup/FirebaseUnitTest.java
+++ app/src/test/java/ch/epfl/sweng/studyup/FirebaseUnitTest.java
@@ -2,11 +2,40 @@ package ch.epfl.sweng.studyup;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 public class FirebaseUnitTest {
     @Test(expected = IllegalArgumentException.class)
     public void wrongSciper() {
-        Firebase.get().getUser(0);
-        Firebase.get().getUser(999999999);
-        Firebase.get().getUser(-42);
+        Map<String, Object> thing;
+        thing = Firebase.get().getUserData(0);
+        thing = Firebase.get().getUserData(999999999);
+        thing = Firebase.get().getUserData(-42);
+    }
+
+    @Test
+    public void correctData() {
+        Map<String, Object> thing = Firebase.get().getUserData(0);
+
+        Map<String, Object> expected = new HashMap<>();
+
+        Map<String, String> name = new HashMap<>();
+        name.put("firstname", "Jean-Louis");
+        name.put("lastname", "Reymond");
+
+        expected.put("name", name);
+        expected.put("section", "IN");
+        expected.put("year", "BA1");
+
+        assertThat(thing, is(expected));
+
+        assertEquals(thing.get(name), expected.get(name));
+        assertEquals(thing.get("section"), expected.get("section"));
+        assertEquals(thing.get("year"), expected.get("year"));
     }
 }
