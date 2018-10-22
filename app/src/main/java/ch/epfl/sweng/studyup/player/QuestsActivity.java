0f939f2223987950af237e1a67ef04d30df9586a
Jules Gottraux <jules.gottraux@epfl.ch>
2018-10-22 15:37:14 +0200

    Add of listView that display the questions

--- app/src/main/java/ch/epfl/sweng/studyup/player/QuestsActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/player/QuestsActivity.java
@@ -1,15 +1,29 @@
 package ch.epfl.sweng.studyup.player;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
+import ch.epfl.sweng.studyup.MainActivity;
 import ch.epfl.sweng.studyup.R;
+import ch.epfl.sweng.studyup.questions.DisplayQuestionActivity;
+import ch.epfl.sweng.studyup.questions.Question;
 import ch.epfl.sweng.studyup.utils.Navigation;
 import ch.epfl.sweng.studyup.utils.Utils;
+
+import static ch.epfl.sweng.studyup.questions.QuestionParser.parseQuestions;
 
 /**
  * QuestsActivity
@@ -17,7 +31,7 @@ import ch.epfl.sweng.studyup.utils.Utils;
  * Quests.
  */
 public class QuestsActivity extends Navigation {
-
+    List<Question> questions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +42,29 @@ public class QuestsActivity extends Navigation {
         getSupportActionBar().setTitle(null);
 
         navigationSwitcher(QuestsActivity.this, QuestsActivity.class, Utils.QUESTS_INDEX);
+
+        final List<Question> questions = parseQuestions(this.getApplicationContext(), false);
+        int nbrQuestion = questions.size();
+        String[] list = new String[nbrQuestion];
+        for(int i = 0; i < nbrQuestion; ++i) {
+            if(questions.get(i).isTrueFalseQuestion()) {
+                list[i] = "Question "+i+": true or false";
+            } else {
+                list[i] = "Question "+i+": multiple choice";
+            }
+
+        }
+
+        ListView listView = findViewById(R.id.listViewQuests);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(DisplayQuestionActivity.getIntentForDisplayQuestion(parent.getContext(), questions.get(position)));
+            }
+        });
     }
 
     // Display the toolbar
