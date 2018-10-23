9cea9ac8abd1954371dfe37365c45ce950bf2d2a
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-10-23 11:49:16 +0200

    Fix imports ServiceBackgroundLocationTest

--- app/src/androidTest/java/ch/epfl/sweng/studyup/ServiceBackgroundLocationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/ServiceBackgroundLocationTest.java
@@ -7,6 +7,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ch.epfl.sweng.studyup.map.BackgroundLocation;
+
 
 @RunWith(AndroidJUnit4.class)
 public class ServiceBackgroundLocationTest {
