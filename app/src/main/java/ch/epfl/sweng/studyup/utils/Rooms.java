445405ea45449560ded9d973c070cb682787f208
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-10-26 09:38:28 +0200

    Change initialization of ROOMS_LOCATIONS

--- app/src/main/java/ch/epfl/sweng/studyup/utils/Rooms.java
+++ app/src/main/java/ch/epfl/sweng/studyup/utils/Rooms.java
@@ -11,40 +11,35 @@ import ch.epfl.sweng.studyup.map.Room;
 public class Rooms {
     private final static double RADIUS_ROOM = 30.0;
 
-    public final static Map<String, Room> ROOMS_LOCATIONS;
+    public final static Map<String, Room> ROOMS_LOCATIONS = new HashMap<String, Room>(){
+        {
+            put("CE_1_1", new Room(46.5206650, 6.5693740));
+            put("CE_1_2", new Room(46.5204350, 6.5693790));
+            put("CE_1_3", new Room(46.5206680, 6.5697490));
+            put("CE_1_4", new Room(46.5204010, 6.5697540));
+            put("CE_1_5", new Room(46.5203640, 6.5705050));
+            put("CE_1_6", new Room(46.5204850, 6.5707300));
 
-    static {
-        HashMap<String, Room> locations = new HashMap<String, Room>() {
-            {
-                put("CE_1_1", new Room(46.5206650, 6.5693740));
-                put("CE_1_2", new Room(46.5204350, 6.5693790));
-                put("CE_1_3", new Room(46.5206680, 6.5697490));
-                put("CE_1_4", new Room(46.5204010, 6.5697540));
-                put("CE_1_5", new Room(46.5203640, 6.5705050));
-                put("CE_1_6", new Room(46.5204850, 6.5707300));
+            put("CM_1_1", new Room(46.5206550, 6.5674980));
+            put("CM_1_2", new Room(46.5203840, 6.5675020));
+            put("CM_1_3", new Room(46.5203810, 6.5671270));
+            put("CM_1_4", new Room(46.5204150, 6.5665410));
+            put("CM_1_5", new Room(46.5204140, 6.5664010));
 
-                put("CM_1_1", new Room(46.5206550, 6.5674980));
-                put("CM_1_2", new Room(46.5203840, 6.5675020));
-                put("CM_1_3", new Room(46.5203810, 6.5671270));
-                put("CM_1_4", new Room(46.5204150, 6.5665410));
-                put("CM_1_5", new Room(46.5204140, 6.5664010));
+            put("CO_1_1", new Room(46.5199270, 6.5653150));
+            put("CO_1_2", new Room(46.5199070, 6.5644510));
+            put("CO_1_3", new Room(46.5199070, 6.5644510));
+            put("CO_1_4", new Room(46.5201620, 6.5648470));
+            put("CO_1_5", new Room(46.5201610, 6.5646430));
+            put("CO_1_6", new Room(46.5201480, 6.5643660));
 
-                put("CO_1_1", new Room(46.5199270, 6.5653150));
-                put("CO_1_2", new Room(46.5199070, 6.5644510));
-                put("CO_1_3", new Room(46.5199070, 6.5644510));
-                put("CO_1_4", new Room(46.5201620, 6.5648470));
-                put("CO_1_5", new Room(46.5201610, 6.5646430));
-                put("CO_1_6", new Room(46.5201480, 6.5643660));
+            put("INN_3_26", new Room(46.5187270, 6.5625000));
 
-                put("INN_3_26", new Room(46.5187270, 6.5625000));
+            put("INR_0_11", new Room(46.5184010, 6.5627920));
 
-                put("INR_0_11", new Room(46.5184010, 6.5627920));
-
-                put("BC_0_0", new Room(46.5185229, 6.5619692));
-            }
-        };
-        ROOMS_LOCATIONS = Collections.unmodifiableMap(locations);
-    }
+            put("BC_0_0", new Room(46.5185229, 6.5619692));
+        }
+    };
 
     //https://stackoverflow.com/questions/8832071/how-can-i-get-the-distance-between-two-point-by-latlng
     public static double distanceBetweenTwoLatLng(LatLng latLng1, LatLng latLng2){
