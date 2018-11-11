a31e0cb3aefad1b46c5533423d0b27bc0f4c92cf
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-11 12:31:14 +0100

    Add item Coin_Sack

--- app/src/main/java/ch/epfl/sweng/studyup/items/Items.java
+++ app/src/main/java/ch/epfl/sweng/studyup/items/Items.java
@@ -4,12 +4,16 @@ import ch.epfl.sweng.studyup.player.Player;
 import ch.epfl.sweng.studyup.utils.Utils;
 
 public enum Items {
-    XP_POTION;
+    XP_POTION,
+    COIN_SACK;
 
     public static void onConsume(Items item){
         switch (item){
             case XP_POTION:
                 Player.get().addExperience(Utils.XP_STEP, Utils.mainActivity);
+                break;
+            case COIN_SACK:
+                Player.get().addCurrency(Utils.CURRENCY_PER_LEVEL, Utils.mainActivity);
                 break;
             default:
                 throw new IllegalArgumentException("Unknown item");
