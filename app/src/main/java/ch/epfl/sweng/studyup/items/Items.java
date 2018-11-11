d8dbf3e8559554280ccc32651e63e3cb69da92da
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-11 12:41:12 +0100

    Add description to items

--- app/src/main/java/ch/epfl/sweng/studyup/items/Items.java
+++ app/src/main/java/ch/epfl/sweng/studyup/items/Items.java
@@ -7,6 +7,9 @@ public enum Items {
     XP_POTION,
     COIN_SACK;
 
+    private static final String XP_POTION_DESCRITPION = "A potion that gives you some xp when drunk !";
+    private static final String COIN_SACK_DESCRITPION = "A sack containing lots of shiny coins !";
+
     public static void onConsume(Items item){
         switch (item){
             case XP_POTION:
@@ -15,6 +18,17 @@ public enum Items {
             case COIN_SACK:
                 Player.get().addCurrency(Utils.CURRENCY_PER_LEVEL, Utils.mainActivity);
                 break;
+            default:
+                throw new IllegalArgumentException("Unknown item");
+        }
+    }
+
+    public static String getDescription(Items item){
+        switch (item){
+            case XP_POTION:
+                return XP_POTION_DESCRITPION;
+            case COIN_SACK:
+                return COIN_SACK_DESCRITPION;
             default:
                 throw new IllegalArgumentException("Unknown item");
         }
