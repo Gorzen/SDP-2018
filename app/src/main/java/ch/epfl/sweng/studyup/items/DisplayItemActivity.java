a96e290f636e020ef0f03c17f9d460baf1d80a35
Florian Ravasi <florian.ravasi@epfl.ch>
2018-11-11 14:42:24 +0100

    Add displayItemActivity that displays an image with a description of the chosen item

--- app/src/main/java/ch/epfl/sweng/studyup/items/DisplayItemActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/items/DisplayItemActivity.java
@@ -1,4 +1,27 @@
 package ch.epfl.sweng.studyup.items;
 
-public class DisplayItemActivity {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ch.epfl.sweng.studyup.R;
+
+public class DisplayItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_item);
+
+        Intent intent = getIntent();
+        Items item = Items.getItems(intent.getStringExtra(DisplayItemActivity.class.getName()));
+        TextView textViewName = findViewById(R.id.item_name);
+        textViewName.setText(Items.getName(item));
+        TextView textViewDescritpion = findViewById(R.id.item_description);
+        textViewDescritpion.setText(Items.getDescription(item));
+        ImageView img = findViewById(R.id.item_image);
+        img.setImageResource(R.drawable.coin_sack);
+    }
 }
--- app/src/main/java/ch/epfl/sweng/studyup/items/InventoryActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/items/InventoryActivity.java
@@ -1,9 +1,12 @@
 package ch.epfl.sweng.studyup.items;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,11 +15,12 @@ import java.util.List;
 
 import ch.epfl.sweng.studyup.R;
 import ch.epfl.sweng.studyup.player.Player;
+import ch.epfl.sweng.studyup.questions.DisplayQuestionActivity;
 import ch.epfl.sweng.studyup.utils.navigation.NavigationStudent;
 import ch.epfl.sweng.studyup.utils.Utils;
 
 public class InventoryActivity extends NavigationStudent {
-
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,15 @@ public class InventoryActivity extends NavigationStudent {
 
         ArrayList<String> itemsName = getItemsNames();
         ListView listView = findViewById(R.id.listViewItems);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsName);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsName);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(parent.getContext(), DisplayItemActivity.class).putExtra(DisplayItemActivity.class.getName(), adapter.getItem(position)));
+            }
+        });
     }
 
 
