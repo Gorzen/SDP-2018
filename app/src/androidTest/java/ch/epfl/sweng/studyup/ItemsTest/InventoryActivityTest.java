355dffaf115f8b89262c02bb875f7f105f24bcfa
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-12 11:03:07 +0100

    Add @RunWith to tests

--- app/src/androidTest/java/ch/epfl/sweng/studyup/ItemsTest/InventoryActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/ItemsTest/InventoryActivityTest.java
@@ -2,12 +2,14 @@ package ch.epfl.sweng.studyup.ItemsTest;
 
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.widget.ListView;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,6 +24,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(AndroidJUnit4.class)
 public class InventoryActivityTest {
     ListView list;
 
