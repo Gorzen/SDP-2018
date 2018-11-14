8f84bcebb37a53e3acf7d4a449c3590d9c807568
Jules Gottraux <jules.gottraux@epfl.ch>
2018-11-14 13:49:17 +0100

    Refactor SettingsActivity

--- app/src/main/java/ch/epfl/sweng/studyup/SettingsActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/SettingsActivity.java
@@ -15,32 +15,22 @@ import ch.epfl.sweng.studyup.utils.RefreshContext;
 import static ch.epfl.sweng.studyup.MainActivity.clearCacheToLogOut;
 
 public class SettingsActivity extends RefreshContext {
-
-    private ImageButton logout_button;
-    private Button close_button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        logout_button = (ImageButton) findViewById(R.id.logoutbutton);
-        logout_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-                scheduler.cancel(BackgroundLocation.BACKGROUND_LOCATION_ID);
-                clearCacheToLogOut(SettingsActivity.this);
-                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        close_button = findViewById(R.id.back_button);
-        close_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            }
-        });
+    }
+
+    public void onLogoutClick(View view) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(BackgroundLocation.BACKGROUND_LOCATION_ID);
+        clearCacheToLogOut(SettingsActivity.this);
+        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onBackButton(View view) {
+        finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 }
