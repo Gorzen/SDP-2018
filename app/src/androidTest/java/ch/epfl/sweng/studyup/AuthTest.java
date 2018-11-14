c74716aa5b5be2c0fa55e094edb24ba49455213d
Stache <hugo.hueber@epfl.ch>
2018-11-14 01:37:38 +0100

    Adapted AuthTest

--- app/src/androidTest/java/ch/epfl/sweng/studyup/AuthTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/AuthTest.java
@@ -10,70 +10,42 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.studyup.auth.Authenticator;
-import ch.epfl.sweng.studyup.utils.Utils;
 
+import static ch.epfl.sweng.studyup.utils.GlobalAccessVariables.MOCK_ENABLED;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class AuthTest {
     private static final String TAG = AuthTest.class.getSimpleName();
-
-    @BeforeClass
-    public static void runOnceBeforeClass() {
-        Utils.isMockEnabled = true;
-    }
-
-    @AfterClass
-    public static void runOnceAfterClass() {
-        Utils.isMockEnabled = false;
-    }
-
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void testGreetingFromResponseInvalid() {
-        String invalidResponse = "{\"anykey\":\"anyvalue\"}";
-        assert (Authenticator.getGreetingFromResponse(invalidResponse) == null);
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        MOCK_ENABLED = true;
+    }
+
+    @AfterClass
+    public static void runOnceAfterClass() {
+        MOCK_ENABLED = false;
     }
 
     @Test
-    public void testGreetingFromResponseError() {
-        String invalidResponse = "{\"error\":\"anyvalue\"}";
-        assert (Authenticator.getGreetingFromResponse(invalidResponse) == null);
-    }
-
-
-    @Test
-    public void testGreetingFromResponsePartiallyValid() {
-        String partiallyValidResponse = "{\"Sciper\":\"1234\"}";
-        assertThat(Authenticator.getGreetingFromResponse(partiallyValidResponse), is("Your Sciper number is 1234."));
-    }
-
-    @Test
-    public void testGreetingFromResponseValid() {
-        String validResponse = "{\"Sciper\":\"1234\", \"Firstname\":\"Bob\"}";
-        assertThat(Authenticator.getGreetingFromResponse(validResponse),
-                is("Welcome, Bob.\nYour Sciper number is 1234."));
-    }
-
-
-    @Test
-    public void testTokenFromResponseInvalid() {
+    public void testTokenFromResponseInvalid() throws Exception {
         String invalidResponse = "{\"anykey\":\"anyvalue\"}";
         assert (Authenticator.getTokenFromResponse(invalidResponse) == null);
     }
 
     @Test
-    public void testTokenFromResponseError() {
+    public void testTokenFromResponseError() throws Exception {
         String invalidResponse = "{\"error\":\"anyvalue\"}";
         assert (Authenticator.getTokenFromResponse(invalidResponse) == null);
     }
 
     @Test
-    public void testTokenFromResponseValid() {
+    public void testTokenFromResponseValid() throws Exception {
         String invalidResponse = "{\"access_token\":\"1234\"}";
         assertThat(Authenticator.getTokenFromResponse(invalidResponse), is("1234"));
     }
