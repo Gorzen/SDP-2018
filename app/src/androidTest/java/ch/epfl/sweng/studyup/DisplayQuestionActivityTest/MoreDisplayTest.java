925bd4a20b9c85b1190deb9f1baee899e9befdc2
Jules Gottraux <jules.gottraux@epfl.ch>
2018-11-14 14:30:12 +0100

    Add basic test for display questions

---
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/DisplayQuestionActivityTest/MoreDisplayTest.java
@@ -0,0 +1,28 @@
+package ch.epfl.sweng.studyup.DisplayQuestionActivityTest;
+
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.studyup.questions.Question;
+
+import static ch.epfl.sweng.studyup.questions.DisplayQuestionActivity.DISPLAY_QUESTION_ID;
+import static ch.epfl.sweng.studyup.questions.DisplayQuestionActivity.DISPLAY_QUESTION_TITLE;
+import static ch.epfl.sweng.studyup.questions.DisplayQuestionActivity.getIntentForDisplayQuestion;
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class MoreDisplayTest extends DisplayQuestionActivityTest {
+    @Test
+    public void launchIntentWithoutUriTest(){
+        Intent i = getIntentForDisplayQuestion(InstrumentationRegistry.getTargetContext(), new Question("abc","test", true, 0));
+        i.removeExtra(DISPLAY_QUESTION_ID);
+        mActivityRule.launchActivity(i);
+        assertTrue(mActivityRule.getActivity().isFinishing());
+    }
+
+
+}
---
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/ListItemAdapterTest.java
@@ -0,0 +1,10 @@
+package ch.epfl.sweng.studyup;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ListItemAdapterTest {
+
+}
