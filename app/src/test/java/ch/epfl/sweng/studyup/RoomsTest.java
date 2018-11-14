41655a52ba37f50ba302f21a7e262387e86bee80
Stache <hugo.hueber@epfl.ch>
2018-11-14 01:15:49 +0100

    Adapted RoomsTest

--- app/src/test/java/ch/epfl/sweng/studyup/RoomsTest.java
+++ app/src/test/java/ch/epfl/sweng/studyup/RoomsTest.java
@@ -1,40 +1,37 @@
 package ch.epfl.sweng.studyup;
 
-import android.util.Log;
-
 import com.google.android.gms.maps.model.LatLng;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import ch.epfl.sweng.studyup.map.Room;
 import ch.epfl.sweng.studyup.utils.Rooms;
-import ch.epfl.sweng.studyup.utils.Utils;
 
+import static ch.epfl.sweng.studyup.utils.GlobalAccessVariables.POSITION;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class RoomsTest {
 
     @Test
-    public void computeDistanceTest(){
+    public void computeDistanceTest() {
         double distance = Rooms.distanceBetweenTwoLatLng(Rooms.ROOMS_LOCATIONS.get("INR_0_11").getLocation(), Rooms.ROOMS_LOCATIONS.get("CO_1_1").getLocation());
         assertEquals(256.9647560362717, distance, 1e-4);
     }
 
     @Test
-    public void checkIfUserIsInRoom(){
-        Utils.position = Rooms.ROOMS_LOCATIONS.get("CO_1_1").getLocation();
+    public void checkIfUserIsInRoom() {
+        POSITION = Rooms.ROOMS_LOCATIONS.get("CO_1_1").getLocation();
         assertEquals(true, Rooms.checkIfUserIsInRoom("CO_1_1"));
 
-        Utils.position = new LatLng(46.5185307, 6.5619707);
+        POSITION = new LatLng(46.5185307, 6.5619707);
         assertEquals(true, Rooms.checkIfUserIsInRoom("BC_0_0"));
 
-        Utils.position = new LatLng(0, 0);
+        POSITION = new LatLng(0, 0);
         assertEquals(false, Rooms.checkIfUserIsInRoom("CE_1_6"));
 
-        Utils.position = new LatLng(45, 6);
+        POSITION = new LatLng(45, 6);
         assertEquals(false, Rooms.checkIfUserIsInRoom("CM_1_4"));
     }
 }
