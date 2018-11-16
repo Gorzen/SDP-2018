640fb0a2c9848ec511dadac5bf6a2264d9a36d6f
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-16 12:13:10 +0100

    Coverage Utils, change static classe to abstract

--- app/src/test/java/ch/epfl/sweng/studyup/UtilsTest.java
+++ app/src/test/java/ch/epfl/sweng/studyup/UtilsTest.java
@@ -8,7 +8,13 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ch.epfl.sweng.studyup.utils.Utils;
+
 import static ch.epfl.sweng.studyup.utils.Utils.waitAndTag;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Log.class})
@@ -20,5 +26,20 @@ public class UtilsTest {
         PowerMockito.when(Log.w("FAIL","Test was interrupted: ")).thenThrow(InterruptedException.class);
 
         waitAndTag(500, "FAIL");
+    }
+
+    @Test
+    public void getOrDefaultTest(){
+        Map<String, Object> map = new HashMap<String, Object>(){
+            {
+                put("Test", 1);
+                put("Test2", "test");
+            }
+        };
+
+        assertEquals(1, Utils.getOrDefault(map, "Test", 2));
+        assertEquals("test", Utils.getOrDefault(map, "Test2", 2));
+        assertEquals(true, Utils.getOrDefault(map, "sdf", true));
+
     }
 }
