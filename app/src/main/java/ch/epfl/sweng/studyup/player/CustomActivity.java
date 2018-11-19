0e1efcf77cb9465a5c322b0cb4e3faa65794fb69
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-19 21:39:49 +0100

    Change brown theme, fix CustomActivity mock condition

--- app/src/main/java/ch/epfl/sweng/studyup/player/CustomActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/player/CustomActivity.java
@@ -130,14 +130,14 @@ public class CustomActivity extends NavigationStudent {
 
     private void openCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (MOCK_ENABLED) {
+        if (!MOCK_ENABLED) {
             startActivityForResult(intent, CAMERA);
         }
     }
 
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        if (MOCK_ENABLED) {
+        if (!MOCK_ENABLED) {
             startActivityForResult(intent, GALLERY);
         }
     }
