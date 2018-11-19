75e94956d4efb47ab70a9641d7d74934c62ba5af
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-19 17:43:33 +0100

    User can change App color to blue

--- app/src/main/java/ch/epfl/sweng/studyup/ChooseColorActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/ChooseColorActivity.java
@@ -1,7 +1,24 @@
 package ch.epfl.sweng.studyup;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import ch.epfl.sweng.studyup.utils.GlobalAccessVariables;
 import ch.epfl.sweng.studyup.utils.RefreshContext;
 
 public class ChooseColorActivity extends RefreshContext {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_color);
+    }
 
+    public void changeTheme(View view){
+        //Change les boutons pour soit AppTheme soit AppThemeBlue
+        GlobalAccessVariables.APP_THEME = R.style.AppThemeBlue;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
 }
