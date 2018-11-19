369f7b64f3124115dbfbec0d9dbaeea3ff19dc17
Florian Ravasi <florian.ravasi@epfl.ch>
2018-11-19 10:55:05 +0100

    Increase thread sleep to 10s

--- app/src/androidTest/java/ch/epfl/sweng/studyup/AAAEditQuestionActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/AAAEditQuestionActivityTest.java
@@ -58,13 +58,13 @@ public class AAAEditQuestionActivityTest {
     public void editTrueFalseQuestionAnswer0to1() {
         q = new Question(questionUUID, "True false test", true, 0, Constants.Course.SWENG.name());
         Firestore.get().addQuestion(q);
-        Utils.waitAndTag(3000, this.getClass().getName());
+        Utils.waitAndTag(10000, this.getClass().getName());
         clickOnListViewItem();
         onView(withId(R.id.radio_answer2)).perform(click());
         onView(withId(R.id.addQuestionButton)).perform(click());
         Firestore.get().loadQuestions(mActivityRule.getActivity());
         assertEquals(1, 1);
-       LiveData<List<Question>> parsedList = QuestionParser.parseQuestionsLiveData(mActivityRule.getActivity().getApplicationContext());
+      // LiveData<List<Question>> parsedList = QuestionParser.parseQuestionsLiveData(mActivityRule.getActivity().getApplicationContext());
 
        /* parsedList.observe(mActivityRule.getActivity(), new Observer<List<Question>>() {
             @Override
