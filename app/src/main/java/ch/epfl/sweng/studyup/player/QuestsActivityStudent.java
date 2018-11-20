96ed85d1ec2dbcf7717e357815622b64b8662914
Jules Gottraux <jules.gottraux@epfl.ch>
2018-11-20 15:02:00 +0100

    Add change in quest student adapter and related test

--- app/src/main/java/ch/epfl/sweng/studyup/player/QuestsActivityStudent.java
+++ app/src/main/java/ch/epfl/sweng/studyup/player/QuestsActivityStudent.java
@@ -2,14 +2,17 @@ package ch.epfl.sweng.studyup.player;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +23,9 @@ import ch.epfl.sweng.studyup.R;
 import ch.epfl.sweng.studyup.questions.DisplayQuestionActivity;
 import ch.epfl.sweng.studyup.questions.Question;
 import ch.epfl.sweng.studyup.utils.navigation.NavigationStudent;
-import static ch.epfl.sweng.studyup.utils.Constants.*;
 
-import ch.epfl.sweng.studyup.utils.QuestListViewAdapter;
 import static ch.epfl.sweng.studyup.questions.QuestionParser.parseQuestionsLiveData;
+import static ch.epfl.sweng.studyup.utils.Constants.QUESTS_INDEX_STUDENT;
 
 public class QuestsActivityStudent extends NavigationStudent {
 
@@ -33,7 +35,7 @@ public class QuestsActivityStudent extends NavigationStudent {
         setContentView(R.layout.activity_quests_student);
         navigationSwitcher(QuestsActivityStudent.this, QuestsActivityStudent.class, QUESTS_INDEX_STUDENT);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
 
@@ -47,16 +49,12 @@ public class QuestsActivityStudent extends NavigationStudent {
     }
 
     private void setupListView(final List<Question> quests) {
-        ArrayList<String> listTitle = new ArrayList<>();
-        ArrayList<Integer> listImageID = new ArrayList<>();
+        List<Integer> listImageID = new ArrayList<>();
 
         Map<String, Boolean> answeredQuestion = Player.get().getAnsweredQuestion();
         Set<String> answeredQuestionId = answeredQuestion == null ? null : answeredQuestion.keySet();
 
         for(Question q: quests) {
-            listTitle.add(q.getTitle());
-
-
             if(answeredQuestion == null || !answeredQuestionId.contains(q.getQuestionId())) {
                 listImageID.add(R.drawable.ic_todo_grey_24dp);
             } else if(answeredQuestion.get(q.getQuestionId())) {
@@ -66,12 +64,12 @@ public class QuestsActivityStudent extends NavigationStudent {
             }
         }
 
-        setupOnClickListenerListView(quests, listTitle, listImageID);
+        setupOnClickListenerListView(quests, quests, listImageID);
     }
 
-    private void setupOnClickListenerListView(final List<Question> quests, ArrayList<String> listTitle, ArrayList<Integer> listImageID) {
+    private void setupOnClickListenerListView(final List<Question> quests, List<Question> questions, List<Integer> listImageID) {
         ListView listView = findViewById(R.id.listViewQuests);
-        QuestListViewAdapter adapter = new QuestListViewAdapter(this, listTitle, listImageID);
+        QuestListViewAdapterStudent adapter = new QuestListViewAdapterStudent(this, R.layout.quest_list_view_student_model, questions, listImageID);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -82,5 +80,46 @@ public class QuestsActivityStudent extends NavigationStudent {
 
             }
         });
+    }
+
+    public class QuestListViewAdapterStudent extends BaseAdapter {
+        private Context cnx;
+        private List<Question> questions;
+        private int idLayout;
+        List<Integer> ids;
+
+        public QuestListViewAdapterStudent(Context cnx, int idLayout, List<Question> questions, List<Integer> ids) {
+            this.cnx=cnx;
+            this.questions = questions;
+            this.idLayout = idLayout;
+            this.ids = ids;
+        }
+
+        @Override
+        public int getCount() {
+            return questions.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return questions.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return ids.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                convertView=View.inflate(cnx, idLayout, null);
+            }
+            TextView text_view = convertView.findViewById(R.id.quest_title);
+            text_view.setText(questions.get(position).getTitle());
+            ImageView image_view = convertView.findViewById(R.id.is_quest_done);
+            image_view.setImageResource(ids.get(position));
+            return convertView;
+        }
     }
 }
