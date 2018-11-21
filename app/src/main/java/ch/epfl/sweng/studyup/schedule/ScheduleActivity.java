0148183c8968a6020245cc17ee2a4f22e05bb13d
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-21 14:06:13 +0100

    Add sweng course to schedule and 'Now line'

--- app/src/main/java/ch/epfl/sweng/studyup/schedule/ScheduleActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/schedule/ScheduleActivity.java
@@ -12,8 +12,10 @@ import android.view.View;
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
+import com.alamkanak.weekview.WeekViewLoader;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import ch.epfl.sweng.studyup.R;
@@ -23,6 +25,26 @@ import ch.epfl.sweng.studyup.utils.navigation.NavigationStudent;
 import static ch.epfl.sweng.studyup.utils.Constants.INVENTORY_INDEX;
 
 public class ScheduleActivity extends NavigationStudent {
+    private final MonthLoader.MonthChangeListener monthChangeListener = new MonthLoader.MonthChangeListener() {
+        @Override
+        public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+            return new ArrayList<>();
+        }
+    };
+
+    private final WeekView.EventClickListener eventClickListener = new WeekView.EventClickListener() {
+        @Override
+        public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+        }
+    };
+
+    private final WeekView.EventLongPressListener eventLongPressListener = new WeekView.EventLongPressListener() {
+        @Override
+        public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +53,22 @@ public class ScheduleActivity extends NavigationStudent {
 
         WeekView mWeekView = findViewById(R.id.weekView);
 
-        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+        mWeekView.setMonthChangeListener(monthChangeListener);
+        mWeekView.setOnEventClickListener(eventClickListener);
+        mWeekView.setEventLongPressListener(eventLongPressListener);
+        mWeekView.setWeekViewLoader(new WeekViewLoader() {
             @Override
-            public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                return new ArrayList<>();
+            public double toWeekViewPeriodIndex(Calendar instance) {
+                return 0;
             }
-        });
 
-        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
             @Override
-            public void onEventClick(WeekViewEvent event, RectF eventRect) {
-
-            }
-        });
-
-        mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
-            @Override
-            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
-
+            public List<? extends WeekViewEvent> onLoad(int periodIndex) {
+                return new ArrayList<WeekViewEvent>(){
+                    {
+                        add(new WeekViewEvent(1, "Sweng", 2018, 11, 22, 8, 15, 2018, 11, 22, 10, 0));
+                    }
+                };
             }
         });
     }
