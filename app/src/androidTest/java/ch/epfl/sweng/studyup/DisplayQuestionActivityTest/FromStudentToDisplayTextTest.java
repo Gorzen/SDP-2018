f5f61d797c90d39347b193209437cf6e11f86503
Jules Gottraux <jules.gottraux@epfl.ch>
2018-11-21 02:04:26 +0100

    Add lucas' unit test, make test to check text's display work

--- app/src/androidTest/java/ch/epfl/sweng/studyup/DisplayQuestionActivityTest/FromStudentToDisplayTextTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/DisplayQuestionActivityTest/FromStudentToDisplayTextTest.java
@@ -20,7 +20,9 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.studyup.utils.Constants.XP_STEP;
 import static ch.epfl.sweng.studyup.utils.GlobalAccessVariables.MOCK_UUID;
 import static ch.epfl.sweng.studyup.utils.Utils.waitAndTag;
@@ -60,8 +62,9 @@ public class FromStudentToDisplayTextTest {
                 onData(anything()).inAdapterView(withId(R.id.listViewQuests))
                         .atPosition(i)
                         .perform(click());
-                waitAndTag(2000, TAG);
+                waitAndTag(4000, TAG);
 
+                onView(withId(R.id.question_text_display)).check(matches(withText("Short text\n")));
             }
         }
     }
