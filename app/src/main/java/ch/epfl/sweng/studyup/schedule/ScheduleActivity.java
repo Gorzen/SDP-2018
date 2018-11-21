787ae59fd3f6ec33f7d0e47b4aaff51665bdcea7
Lucien Michael Iseli <lucien.iseli93@gmail.com>
2018-11-21 13:44:25 +0100

    Schedule activity displays empty schedule

--- app/src/main/java/ch/epfl/sweng/studyup/schedule/ScheduleActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/schedule/ScheduleActivity.java
@@ -1,11 +1,20 @@
 package ch.epfl.sweng.studyup.schedule;
 
+import android.graphics.RectF;
+import android.media.MediaDrm;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.sweng.studyup.R;
 import ch.epfl.sweng.studyup.items.InventoryActivity;
@@ -19,12 +28,29 @@ public class ScheduleActivity extends NavigationStudent {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(null);
 
-        navigationSwitcher(ScheduleActivity.this, InventoryActivity.class, INVENTORY_INDEX);
+        WeekView mWeekView = findViewById(R.id.weekView);
 
+        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                return new ArrayList<>();
+            }
+        });
+
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
+            @Override
+            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
     }
 
 }
