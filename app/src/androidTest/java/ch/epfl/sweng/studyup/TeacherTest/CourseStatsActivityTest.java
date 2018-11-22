e3af734ffd946da536c6de525df86aad8d309f09
Lucas Strauss <lucas.strauss@epfl.ch>
2018-11-22 16:22:12 +0100

    work in progress

--- app/src/androidTest/java/ch/epfl/sweng/studyup/TeacherTest/CourseStatsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/TeacherTest/CourseStatsActivityTest.java
@@ -3,7 +3,6 @@ package ch.epfl.sweng.studyup.TeacherTest;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
@@ -24,14 +23,15 @@ import java.util.List;
 import ch.epfl.sweng.studyup.R;
 import ch.epfl.sweng.studyup.player.Player;
 import ch.epfl.sweng.studyup.teacher.CourseStatsActivity;
+import ch.epfl.sweng.studyup.teacher.DisplayCourseStatsActivity;
 import ch.epfl.sweng.studyup.utils.Constants;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static ch.epfl.sweng.studyup.utils.GlobalAccessVariables.MOCK_ENABLED;
 import static org.hamcrest.CoreMatchers.anything;
 
 @RunWith(AndroidJUnit4.class)
@@ -61,7 +61,7 @@ public class CourseStatsActivityTest {
     public void onClickTest(){
         cleanAndAdd();
         onData(anything()).inAdapterView(withId(R.id.listViewQuests)).atPosition(0).perform(click());
-        Intents.intending()
+        Intents.intended(hasComponent(DisplayCourseStatsActivity.class.getName()));
     }
 
     private void cleanAndAdd(){
