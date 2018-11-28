f8db84f2571392db1ce7c4b6010ce4b3c4048bb8
Léopold Bouraux <leopold.bouraux@epfl.ch>
2018-11-28 13:11:27 +0100

    debug the stat pb

--- app/src/main/java/ch/epfl/sweng/studyup/questions/AddOrEditQuestionActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/questions/AddOrEditQuestionActivity.java
@@ -401,7 +401,7 @@ public class AddOrEditQuestionActivity extends NavigationStudent {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 for (Course c : courses){
-                    if(which == c.ordinal()){
+                    if(which == c.ordinal()-1){
                         chosenCourse = c;
                         view_chosen_course.setText(getString(R.string.chosen_course_for_question)+c.toString());
                     }
