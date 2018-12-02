6c73ee90237b55f580e261b96aa11c59076617cb
Florian Ravasi <florian.ravasi@epfl.ch>
2018-12-02 15:16:56 +0100

    Add missing NPCActivity class

---
+++ app/src/main/java/ch/epfl/sweng/studyup/npc/NPCActivity.java
@@ -0,0 +1,15 @@
+package ch.epfl.sweng.studyup.npc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.epfl.sweng.studyup.R;
+
+public class NPCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_npc);
+    }
+}
