30a13beec467e4e0464cbbda250d34beb44c1c8e
Jules Gottraux <jules.gottraux@epfl.ch>
2018-12-03 15:52:46 +0100

    Add test to change role

--- app/src/androidTest/java/ch/epfl/sweng/studyup/SettingsTest/SettingsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/SettingsTest/SettingsActivityTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.studyup.SettingsTest;
 
+import android.content.Intent;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
@@ -16,6 +17,11 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import ch.epfl.sweng.studyup.LoginActivity;
 import ch.epfl.sweng.studyup.R;
@@ -24,6 +30,7 @@ import ch.epfl.sweng.studyup.player.Player;
 import ch.epfl.sweng.studyup.settings.ChooseColorActivity;
 import ch.epfl.sweng.studyup.settings.CourseSelectionActivity;
 import ch.epfl.sweng.studyup.settings.SettingsActivity;
+import ch.epfl.sweng.studyup.teacher.QuestsActivityTeacher;
 import ch.epfl.sweng.studyup.utils.Constants;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -63,18 +70,33 @@ public class SettingsActivityTest {
         Intents.release();
     }
 
-    //@Test
+    @Test
     public void logoutButton() {
-        FileCacher<String[]> persistLogin = new FileCacher<>(mActivityRule.getActivity(), PERSIST_LOGIN_FILENAME);
+        FileCacher<List<String>> persistLogin = new FileCacher<>(mActivityRule.getActivity(), PERSIST_LOGIN_FILENAME);
         try {
-            String[] s = {"Test"};
-            persistLogin.writeCache(s);
+            List<String> data = new ArrayList<>(Collections.singletonList("Test"));
+            persistLogin.writeCache(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
         onView(ViewMatchers.withId(R.id.logout_button)).perform(click());
         assertTrue(!persistLogin.hasCache());
         intended(hasComponent(LoginActivity.class.getName()));
+    }
+
+    @Test
+    public void changeRoleToTeacherTest() {
+        onView(withId(R.id.changeRoleButton)).perform(click());
+        intended(hasComponent(QuestsActivityTeacher.class.getName()));
+    }
+
+    @Test
+    public void changeRoleToStudentTest() {
+        Player.get().setRole(Constants.Role.teacher);
+        mActivityRule.finishActivity();
+        mActivityRule.launchActivity(new Intent());
+        onView(withId(R.id.changeRoleButton)).perform(click());
+        intended(hasComponent(HomeActivity.class.getName()));
     }
 
     @Test
