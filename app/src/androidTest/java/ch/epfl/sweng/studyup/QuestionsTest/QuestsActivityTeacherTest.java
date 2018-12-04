0993561e512e985536b7f81a3fb5a82d486e2dfa
Jules Gottraux <jules.gottraux@epfl.ch>
2018-12-04 11:19:26 +0100

    Modify slightly delete 	question test

--- app/src/androidTest/java/ch/epfl/sweng/studyup/QuestionsTest/QuestsActivityTeacherTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/studyup/QuestionsTest/QuestsActivityTeacherTest.java
@@ -170,9 +170,7 @@ public class QuestsActivityTeacherTest {
                 onView(withText(R.string.yes_upper)).inRoot(isDialog())
                         .check(matches(isDisplayed()))
                         .perform(click());
-                Utils.waitAndTag(2000, TAG);
-                Firestore.get().loadQuestions(rule.getActivity());
-                Utils.waitAndTag(2000, TAG);
+                Utils.waitAndTag(10000, TAG);
             }
         } catch (Exception e) {}
     }
