2e55ae554edfe6fa72a658333de686a5ccc21751
Léopold Bouraux <leopold.bouraux@epfl.ch>
2018-12-07 04:08:09 +0100

    back button added

--- app/src/main/java/ch/epfl/sweng/studyup/settings/AboutUsActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/settings/AboutUsActivity.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.studyup.settings;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ch.epfl.sweng.studyup.R;
+import ch.epfl.sweng.studyup.items.InventoryActivity;
+import ch.epfl.sweng.studyup.items.ShopActivity;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -11,5 +15,9 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+
+    public void onBackSettings(View view) {
+        startActivity(new Intent(AboutUsActivity.this, InventoryActivity.class));
     }
 }
