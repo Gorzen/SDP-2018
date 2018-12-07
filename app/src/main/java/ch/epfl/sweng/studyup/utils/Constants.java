3610aee9246b19c291fd7a5a8636d88dad3c912b
Jules Gottraux <jules.gottraux@epfl.ch>
2018-12-07 16:22:11 +0100

    Add scrollView in Home

--- app/src/main/java/ch/epfl/sweng/studyup/utils/Constants.java
+++ app/src/main/java/ch/epfl/sweng/studyup/utils/Constants.java
@@ -66,7 +66,7 @@ public abstract class Constants {
             FB_USERS, FB_FIRSTNAME, FB_LASTNAME, FB_SCIPER, FB_ROLE, FB_XP, FB_CURRENCY,
             FB_LEVEL, FB_SECTION, FB_YEAR, FB_TOKEN, FB_QUESTIONS, FB_QUESTS, FB_USERNAME, FB_ITEMS, FB_ANSWERED_QUESTIONS);
     public static final Set<String> SUPER_USERS = Sets.newHashSet(
-            "262413", "272432", "274999", "300137", "245940");
+            "262413", "272432", "300137", "245940");
 
 
     // Values associated with Firebase storage
