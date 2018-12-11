16710e42f7ce6af161876836335bab2087b4cb92
Léopold Bouraux <leopold.bouraux@epfl.ch>
2018-12-12 00:32:34 +0100

    re-add authentification layout

--- app/src/main/java/ch/epfl/sweng/studyup/auth/AuthenticationActivity.java
+++ app/src/main/java/ch/epfl/sweng/studyup/auth/AuthenticationActivity.java
@@ -140,7 +140,7 @@ public class AuthenticationActivity extends RefreshContext {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_authentification);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
